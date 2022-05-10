e756f88af2000d730873a85f47a3bd9cecb1e341
yousefelnaggar <105175925+yousefelnaggar@users.noreply.github.com>
2022-05-11 01:33:25 +0200

    Add files via upload

--- js/horror.js
+++ js/horror.js
@@ -1,9 +1,19 @@
 alert("welcome")
-function download(){
-window.location.href="https://www.w3schools.com/html/tryit.asp?filename=tryhtml_iframe_target"
-}
 function hid(){
-    var x=document.getElementById("ff");
-    
-        x.style.display="block";}
+document.getElementById("ff").style.display="block";
+   }
+function hidd(){
+document.getElementById("f").style.display="block";
+   }
+
+function hiidd(){
+document.getElementById("fff").style.display="block";
+   }
+
+function hhiidd(){
+document.getElementById("f0").style.display="block";
+                                }
+
+
+     
     
