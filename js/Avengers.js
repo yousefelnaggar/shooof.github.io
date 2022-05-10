a41662123f2abda9e3f174b0c7c8f4f86c809d5a
yousefelnaggar <105175925+yousefelnaggar@users.noreply.github.com>
2022-05-10 18:39:44 +0200

    Update Avengers.js

--- js/Avengers.js
+++ js/Avengers.js
@@ -5,3 +5,17 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
