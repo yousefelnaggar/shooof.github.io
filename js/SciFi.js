d973fe5ec2b208e396ab2dd69fcb8e89ab60d239
yousefelnaggar <105175925+yousefelnaggar@users.noreply.github.com>
2022-05-10 18:40:37 +0200

    Update SciFi.js

--- js/SciFi.js
+++ js/SciFi.js
@@ -5,8 +5,3 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
-function myfunction() {
-    let x = document.getElementsByid("summ");
-    x.style.fontSize = "22px"; 
-    x.style.color = "yellow"; 
- }
