b3fc2ad04ba03718d6a5d2e909f80a8cb0c7bf07
yousefelnaggar <105175925+yousefelnaggar@users.noreply.github.com>
2022-05-11 00:33:22 +0200

    Add files via upload

---
+++ anemy.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    let element = document.getElementById("body");
+    element.classList.toggle("Light-modee");
+ }
---
+++ main.js
@@ -0,0 +1,53 @@
+// main-categori
+let x = document.getElementById("categories");
+function categories(){
+  if(x.style.display!="none")
+  {
+    x.style.display="none";
+  }else if(x.style.display=="none"){
+    x.style.display="block";
+  }
+
+}
+function action_page(){
+  window.location.href="egyptianaction.html";
+}
+
+function comedy_page(){
+  window.location.href="egyptiancomedy.html";
+}
+
+function drama_page(){
+  window.location.href="drama.html";
+}
+
+function romantic_page(){
+  window.location.href="romantic.html";
+}
+
+function harrypotter_page(){
+  window.location.href="harrypotter.html";
+}
+
+function cartoon_page(){
+  window.location.href="cartoon.html";
+}
+
+function documentary_page(){
+  window.location.href="documentaries.html";
+}
+function shortmovies_page(){
+  window.location.href="shortmovies.html";
+}
+function horror_page(){
+  window.location.href="horror.html";
+}
+function anemy_page(){
+  window.location.href="anime.html";
+}
+function avengers_page(){
+  window.location.href="Avengers.html";
+}
+function SciFi_page(){
+  window.location.href="SciFi.html";
+}
